8f1192ef2794e04787235d1a3ffc38c379af285c
mmckerns <mmckerns@8bfda07e-5b16-0410-ab1d-fd04ec2748df>
2015-06-29 23:24:54 +0000

    add this ppft wrapper on pp
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/pathos/ppft@795 8bfda07e-5b16-0410-ab1d-fd04ec2748df

---
+++ python2/ppft.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#
+# Author: Mike McKerns (mmckerns @caltech and @uqfoundation)
+# Copyright (c) 2015 California Institute of Technology.
+# License: 3-clause BSD.  The full license text is available at:
+#  - http://trac.mystic.cacr.caltech.edu/project/pathos/browser/dill/LICENSE
+"""
+ppft -- a friendly Parallel Python fork
+"""
+
+from pp import *
+from pp import __version__, _USE_SUBPROCESS, \
+               _Task, _Worker, _RWorker, _Statistics
+import ppauto as auto
+import ppcommon as common
+import pptransport as transport
+import ppworker as worker
+
+# comment out the following if this is a release
+__version__ += '.dev0'
+
+#'''
+#__version__ = __version__.rsplit('.',1)
+#__version__[-1] = str(int(__version__[-1])+1) + '.dev0'
+#__version__ = ".".join(__version__)
+#'''
+
+
+# EOF
