e076ba19404a0611fbbbc64a97f1b0a093033b04
mmckerns <mmckerns@8bfda07e-5b16-0410-ab1d-fd04ec2748df>
2018-01-07 23:07:09 +0000

    update copyright to 2018
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/pathos/ppft@1019 8bfda07e-5b16-0410-ab1d-fd04ec2748df

--- python2/ppauto.py
+++ python2/ppauto.py
@@ -1,7 +1,7 @@
 # Parallel Python Software: http://www.parallelpython.com
 # Copyright (c) 2005-2012 Vitalii Vanovschi.
 # Copyright (c) 2015-2016 California Institute of Technology.
-# Copyright (c) 2016-2017 The Uncertainty Quantification Foundation.
+# Copyright (c) 2016-2018 The Uncertainty Quantification Foundation.
 # All rights reserved.
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -40,7 +40,7 @@ import ppcommon
 
 copyright = """Copyright (c) 2005-2012 Vitalii Vanovschi.
 Copyright (c) 2015-2016 California Institute of Technology.
-Copyright (c) 2016-2017 The Uncertainty Quantification Foundation."""
+Copyright (c) 2016-2018 The Uncertainty Quantification Foundation."""
 __version__ = version = "1.6.4.8"
 
 # broadcast every 10 sec
