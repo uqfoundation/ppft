0154c5772707ffd5fc549877d4029ce28b735892
mmckerns <mmckerns@caltech.edu>
2020-06-15 14:09:21 -0400

    bumping to dill-0.3.2

--- setup.py
+++ setup.py
@@ -125,7 +125,7 @@ Requirements
 Optional requirements::
 
     - ``setuptools``, **version >= 0.6**
-    - ``dill``, **version >= 0.3.1**
+    - ``dill``, **version >= 0.3.2**
 
 
 More Information
@@ -190,7 +190,7 @@ kwds = {
 if has_setuptools:
     kwds.update({
         "zip_safe" : False,
-        "extras_require" : {'dill': ['dill>=0.3.1']},
+        "extras_require" : {'dill': ['dill>=0.3.2']},
     })
 if has_setuptools and pyversion > 2:
     kwds.update({
