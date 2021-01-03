fce21864803def61d4cf11379c71322995378ed2
mmckerns <mmckerns@caltech.edu>
2021-01-03 12:08:10 -0500

    sunset py3.5

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ import sys
 unsupported = None
 if sys.version_info < (2, 7):
     unsupported = 'Versions of Python before 2.7 are not supported'
-elif (3, 0) <= sys.version_info < (3, 5):
-    unsupported = 'Versions of Python before 3.5 are not supported'
+elif (3, 0) <= sys.version_info < (3, 6):
+    unsupported = 'Versions of Python before 3.6 are not supported'
 if unsupported:
     raise ValueError(unsupported)
 
@@ -119,7 +119,7 @@ Requirements
 
 ``ppft`` requires::
 
-    - ``python``, **version == 2.7** or **version >= 3.5**, or ``pypy``
+    - ``python``, **version == 2.7** or **version >= 3.6**, or ``pypy``
     - ``six``, **version >= 1.7.3**
 
 Optional requirements::
@@ -181,7 +181,12 @@ kwds = {
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
         "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Software Development",
         "Topic :: Scientific/Engineering",
         ],
