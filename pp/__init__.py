3ae73b0240cefa3d6ed7a4c752d90d765d6c7039
mmckerns <mmckerns@caltech.edu>
2022-06-16 13:10:15 -0400

    fix #39, add missing imports to pp module

--- pp/__init__.py
+++ pp/__init__.py
@@ -1,4 +1,3 @@
 from ppft import *
-from ppft import __version__, __author__, __doc__, __license__
-
-#FIXME: more imports from ppft.__init__
+from ppft import __version__, __author__, __doc__, __license__, __main__, \
+                 _USE_SUBPROCESS, _Task, _Worker, _RWorker, _Statistics, _pp
