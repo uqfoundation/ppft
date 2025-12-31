0db11448078121cc9fd1d1e12fc8428d94c889bf
Mike McKerns <mmckerns@caltech.edu>
2025-12-30 22:06:08 -0500

    update copyright for 2026, urllib3 to 2.6.0 (#89)

--- ppft/_pp.py
+++ ppft/_pp.py
@@ -1,7 +1,7 @@
 # Parallel Python Software: http://www.parallelpython.com
 # Copyright (c) 2005-2012 Vitalii Vanovschi.
 # Copyright (c) 2015-2016 California Institute of Technology.
-# Copyright (c) 2016-2025 The Uncertainty Quantification Foundation.
+# Copyright (c) 2016-2026 The Uncertainty Quantification Foundation.
 # All rights reserved.
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
